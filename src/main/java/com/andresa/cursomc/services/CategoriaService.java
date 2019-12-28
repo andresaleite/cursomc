4d2948e7f30f344151376b343981b9a8cf888433
Andrêsa Leite <andresaleite@gmail.com>
2019-12-28 15:47:30 -0300

    repository e service de Categoria criados e testados com buscar

---
+++ src/main/java/com/andresa/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.andresa.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andresa.cursomc.domain.Categoria;
+import com.andresa.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
+		
+	}
+}
