c1c6ab1ba07af27e0ad9ae4c975ab549eda4fff8
Andrêsa Leite <andresaleite@gmail.com>
2019-12-28 15:56:03 -0300

    Operação de instanciação, iniciação de categoria

--- src/main/java/com/andresa/cursomc/CursomcApplication.java
+++ src/main/java/com/andresa/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.andresa.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.andresa.cursomc.domain.Categoria;
+import com.andresa.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
