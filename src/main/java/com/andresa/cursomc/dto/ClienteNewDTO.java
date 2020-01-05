8e8b16a0e2b4c25e89b37fdc80b5c55fb844ca58
Andrêsa Leite <andresaleite@gmail.com>
2020-01-05 20:59:55 -0300

    Validação costumizada de CPF e CNPJ na insecao de clientes

--- src/main/java/com/andresa/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/andresa/cursomc/dto/ClienteNewDTO.java
@@ -7,6 +7,9 @@ import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.andresa.cursomc.services.validations.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO  implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -19,17 +22,29 @@ public class ClienteNewDTO  implements Serializable{
 	@Email(message = "E-mail inválido.")
 	private String email;
 	
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	private String cep;
 	
+	@NotEmpty(message = "Preenchimento obrigatório.")
 	private String telefone1;
+	
 	private String telefone2;
+	
 	private String telefone3;
 	
 	private Integer cidadeId;
