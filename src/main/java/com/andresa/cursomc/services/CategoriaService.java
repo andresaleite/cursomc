a23688626440cb11fa56598769aec1dd327686b8
Andrêsa Leite <andresaleite@gmail.com>
2020-01-05 00:14:55 -0300

    Atualização de categoria

--- src/main/java/com/andresa/cursomc/services/CategoriaService.java
+++ src/main/java/com/andresa/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
@@ -24,6 +24,11 @@ public class CategoriaService {
 	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
+		return (Categoria) repo.saveAndFlush(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		this.find(obj.getId());
 		return (Categoria) repo.saveAndFlush(obj);
 	}
 }
