629d6cfbd2c19522315dbeb23258d82b0e33b147
Andrêsa Leite <andresaleite@gmail.com>
2020-01-15 22:05:22 -0300

    validação de e-mail não pode ser repetido na atualização de cliente

--- src/main/java/com/andresa/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/andresa/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.andresa.cursomc.domain.Cliente;
+import com.andresa.cursomc.services.validations.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO  implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
