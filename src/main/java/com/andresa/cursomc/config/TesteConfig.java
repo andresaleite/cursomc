ceec8843481954342d9743e70cd57dc0f8abfcd3
Andrêsa Leite <andresaleite@gmail.com>
2020-01-19 18:29:14 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/andresa/cursomc/config/TesteConfig.java
+++ src/main/java/com/andresa/cursomc/config/TesteConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.andresa.cursomc.services.DBService;
+import com.andresa.cursomc.services.EmailService;
+import com.andresa.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("teste")
@@ -20,6 +22,11 @@ public class TesteConfig {
 	public boolean instantiateDatabase() throws ParseException {
 		dbservice.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 
 }
